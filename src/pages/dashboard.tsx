87bbe43ab5d36725da805db96baf4d4ea0b57f41
Huy Huynh <huy.huynh@dal.ca>
2025-06-06 21:13:28 -0300

    Fix Convex API import path

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "convex/react";
-import { api } from "../../convex/_generated/api";
+import { api } from "~convex/api";
 import { Dashboard } from "@/components/learning/Dashboard";
 import { Loading } from "@/components/ui/loading";
 
